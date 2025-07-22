da1f65c678702487c98955130a4e3020245a6bf3
Muhammad Shakoor <muhammadshakoor86@gmail.com>
2025-07-22 17:31:07 +0500

    add try catch in db.js

--- config/db.js
+++ config/db.js
@@ -9,6 +9,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
 });
 
+try {
+    require('pg');
+    console.log('✅ pg module loaded successfully');
+} catch (err) {
+    console.error('❌ pg module not found:', err);
+}
+
+
 module.exports = sequelize;
 
 
