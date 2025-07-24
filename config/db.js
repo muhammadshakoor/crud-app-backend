e7af2c2e6bc39861a4d553bc8e9d722467c71c51
Jamsheed Saeed <30534494+jamsheedsaeed@users.noreply.github.com>
2025-07-24 16:55:00 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectModule: require('pg'),
     protocol: 'postgres',
     dialectOptions: {
         ssl: { require: true, rejectUnauthorized: false }
