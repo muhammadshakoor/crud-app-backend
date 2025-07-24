3b5363d7b670423ff6f86f7c143b7398998fabd8
Jamsheed Saeed <30534494+jamsheedsaeed@users.noreply.github.com>
2025-07-24 16:56:17 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,19 +8,19 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-// const productRoutes = require('../routes/products.js');
-// const authRoutes = require('../routes/auth.js');
+const productRoutes = require('../routes/products.js');
+const authRoutes = require('../routes/auth.js');
 
 app.get('/', (req, res) => {
     res.send('API is running on Vercel!');
 });
 
-// app.use('/api', productRoutes);
-// app.use('/api/auth', authRoutes);
+app.use('/api', productRoutes);
+app.use('/api/auth', authRoutes);
 
 // Sync DB (but without app.listen)
-// db.sequelize.sync({ alter: true })
-//     .then(() => console.log('Database connected and synced'))
-//     .catch(err => console.error('Database sync error:', err));
+db.sequelize.sync({ alter: true })
+    .then(() => console.log('Database connected and synced'))
+    .catch(err => console.error('Database sync error:', err));
 
 module.exports = app; // ❗ Export for Vercel
