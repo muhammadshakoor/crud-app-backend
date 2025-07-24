8b069fe6a4ca894c44fec05decb8932626f6535d
Muhammad Shakoor <muhammadshakoor86@gmail.com>
2025-07-24 16:11:28 +0500

    routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./models');
+const db = require('../models');
 require('dotenv').config(); // Load .env
 
 const app = express();
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-const productRoutes = require('./routes/products.js');
-const authRoutes = require('./routes/auth.js');
+const productRoutes = require('../routes/products.js');
+const authRoutes = require('../routes/auth.js');
 
 app.get('/', (req, res) => {
-  res.send('API is running on Vercel!');
+    res.send('API is running on Vercel!');
 });
 
 app.use('/api', productRoutes);
@@ -20,7 +20,7 @@ app.use('/api/auth', authRoutes);
 
 // Sync DB (but without app.listen)
 db.sequelize.sync({ alter: true })
-  .then(() => console.log('Database connected and synced'))
-  .catch(err => console.error('Database sync error:', err));
+    .then(() => console.log('Database connected and synced'))
+    .catch(err => console.error('Database sync error:', err));
 
 module.exports = app; // ❗ Export for Vercel
